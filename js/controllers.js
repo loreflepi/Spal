a45941c0076395684a6e1cef1986c8c00ace8bb8
loreflepi <lorena5pinzon@gmail.com>
2017-02-24 16:28:45 -0500

    MD5

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,5 @@
+
+
 app.controller("homeController", function indexController($scope){
 	$scope.usuarios = [
 		{
@@ -17,13 +19,37 @@ app.controller("homeController", function indexController($scope){
  
  
 app.controller("registroController", function ($scope){
+
+
+	$scope.hash=function(n){
+		var hash = hex_md5(n);
+    	return (hash);
+	}
+
 	$scope.enviar=function() {
-	    alert($scope.nombre+ $scope.email+ $scope.colegio);
+	    //alert($scope.nombre+ $scope.email+ $scope.colegio);
+	     alert($scope.hash($scope.contra));
 		}
 })
  
-app.controller("ingresoController", function ingresoController($scope, $location){
-	$scope.saludo = "Hola desde el controlador login";
+app.controller("ingresoController", function ($scope){
+	$scope.guardar=function(){
+		var usuario= document.getElementById("usu").value;
+		var contra= document.getElementById("contra").value;
+		localStorage.setItem("Nombre de usuario", usuario);
+		localStorage.setItem("contraseña", contra);
+		
+	}
+
+	$scope.cargar=function(){
+		/*Obtener datos almacenados*/
+		var usuario = localStorage.getItem("Nombre de usuario");
+		var contra = localStorage.getItem("contraseña");
+		/*Mostrar datos almacenados*/    
+		document.getElementById("usu").innerHTML = usuario;
+		document.getElementById("contra").innerHTML = contra;
+		alert(usuario+" "+contra);
+	}
 	
 })
 
@@ -33,7 +59,7 @@ app.controller("contactoController", function contactoController($scope, $locati
 })
 
 app.controller("inicioController", function ($scope){
-	$scope.saludo = "Hola desde el controlador login";
+	
 	$scope.showDivs=function (n) {
 	  var i;
 	  var x = document.getElementsByClassName("mySlides");
